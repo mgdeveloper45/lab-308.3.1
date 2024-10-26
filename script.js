7c36a87f7d8a16fd17395c8cbf8b7d6f1c345532
MarkGee45 <moel55.mg@gmail.com>
2024-10-26 01:22:04 -0700

    added while to incement initial value if prime is not found

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ for(let i = 0; i <= 100; i++) {
 }
 
 // Part 2: Prime Time
-let n = 4;
-n++
+let n = 9;
+n++;
 
-let isPrime = true;
+while(true) {
+    let isPrime = true;
 
-for(let i = 2; i <= Math.sqrt(n); i++) {
-    // console.log(n)
-    if(n % i === 0) {
-        isPrime = false;
-        break;
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+        // console.log(n)
+        if(n % i === 0) {
+            isPrime = false;
+            break;
+        }
     }
-    
+
     if(isPrime) {
         console.log(n)
         break;
     }
+
+    n++;
 }
